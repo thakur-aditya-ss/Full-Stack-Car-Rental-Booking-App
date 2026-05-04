6bca7ed972ed2bc033feb09cb5f47a4fa392bd72
Aditya Singh <aaasingh1010@gmail.com>
2026-05-04 16:03:10 +0530

    commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/car-rental`)
+        mongoose.connection.on('connected', ()=> console.log("Database Connected Successfully"));
+        
+        // Remove angle brackets if user accidentally left them in from Atlas placeholder
+        const uri = process.env.MONGODB_URI.replace('<', '').replace('>', '');
+        
+        await mongoose.connect(`${uri}/car-rental`)
     } catch (error) {
-        console.log(error.message);
+        console.log("Database Connection Error:", error.message);
     }
 }
 
